e046ed121ad1e266522256b33a85b3611cb2feb4
Evangelina Mercado <angiegalara11@gmail.com>
2024-11-05 21:08:06 -0300

    arrays

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,8 @@
-alert("BIENVENIDOS A NUESTRA APP DE NOTAS")
-let nombreAlumno= prompt ("Ingrese nombre del Alumno");
-let nota1= parseInt(prompt("Ingrese nota del primer parcial"));
-let nota2= parseInt(prompt("Ingrese nota del segundo parcial"));
-let nota3= parseInt(prompt("Ingrese nota del recuperatorio"));
-let inasistencias= parseInt(prompt("Cuántas faltas tuvo el alumno?"));
+let nombreAlumno= document.getElementById(nombreAlumno);
+let nota1= document.getElementById(nota1);
+let nota2= document.getElementById(nota2);
+let nota3= document.getElementById(notaFinal);
+inasistencias= document.getElementById(inasistencias);
 
 let suma= nota1+nota2+nota3;
 let a=nota1;
@@ -16,22 +15,23 @@ console.table(notas)
 
 let añadirNota=true
 while(añadirNota){
-    let nota4= parseInt(prompt("Ingrese Nota Examen Final "));
+    let nota4= document.getElementById(notaFinal);
     
         if(nota4 >=0 && nota4<=10) {
         notas.push (nota4);
         } 
-        let salir= prompt("escriba salir para terminar")
+        //let salir= prompt("escriba salir para terminar")
         if (salir =="salir"){
             añadirNota=false
-            alert ("gracias Profe")
             }
 }
-console.table(notas)
+//console.table(notas)
 
 
 if(nota1>=6){
-    console.log ("APROBÓ PRIMER CUATRIMESTRE");
+            let resultado1= document.getElementById(resultado1)
+            resultado1.innerHTML= "<h3> APROBO PRIMER CUATRIMESTRE </h3>"
+    //console.log ("APROBÓ PRIMER CUATRIMESTRE");
 }
 else{
     console.log ("NO APROBÓ PRIMER CUATRIMESTE")
@@ -52,12 +52,6 @@ function promediar(x,y){
 }
 console.log ("su promedio es "+ promediar(suma,3));
 
-if (promedio>= 7){
-    console.log ("PROMOCIONO LA MATERIA")
-}
-else{
-    console.log ("RINDE EXAMEN FINAL")
-}
 
 
 function informar (nombre, inasistencias){
